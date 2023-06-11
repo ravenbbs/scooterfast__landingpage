2e9e7ee772d6afc9218e297e28bcf447052c5f26
HansUrpay <hansurpayh@gmail.com>
2023-06-11 16:53:40 -0500

    agregando swipper a la seccion home

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Carousel Swiper
+let swiper = new Swiper(".reviews-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
